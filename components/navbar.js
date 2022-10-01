fa81c59e13d1a7666d9957c4b6bc28b95b8a007e
trilochanprasadbhilli <tptrilochanprasad@gmail.com>
2022-10-01 22:14:53 +0530

    navbar.js

---
+++ components/navbar.js
@@ -0,0 +1,706 @@
+function navbar(){
+    return `    <div id="upper_navbar">
+    <div>
+      <img
+        src="https://cdn02.nnnow.com/web-images/master/navtree_metaData/59b2425ae4b0d70964ee66e0/1505806763887/12NNNOWLOGODESKTOP.png"
+      />
+    </div>
+    <span class="line_div">|</span>
+    <div class="nav_atri">
+      <a href=""><i class="fa-solid fa-location-dot"></i> Store Locator</a>
+    </div>
+    <div class="nav_atri">
+      <a href="">
+        <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i
+        ><i class="fa-solid fa-star"></i>
+        <div>Get 10% OFF on your first purchase. Use Code: BEAUTY10</div>
+        <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i
+        ><i class="fa-solid fa-star"></i>
+      </a>
+    </div>
+    <div class="nav_atri">
+      <a href=""><i class="fa-regular fa-down-long"></i> Get APP</a>
+    </div>
+    <span class="line_div">|</span>
+    <div class="nav_atri">
+      <a href=""> <i class="fa-regular fa-box-open"></i> Track Order</a>
+    </div>
+    <span class="line_div">|</span>
+    <div class="nav_atri">
+      <a href=""><i class="fa-solid fa-trophy"></i> Loyalty</a>
+    </div>
+  </div>
+  <div id="middle_navbar">
+    <div>
+      <div id="search">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="Search SEPHORA" />
+        <div id="line"></div>
+      </div>
+    </div>
+    <div>
+      <a href=""
+        ><img
+          src="https://cdn07.nnnow.com/web-images/master/navtree_metaData/59b2657be4b0e6b6e16a9180/1548053082431/se.png"
+        />
+      </a>
+    </div>
+    <div id="logos">
+      <div><i class="fa-regular fa-heart"></i></div>
+      <span class="line_div">|</span>
+      <div>
+        <a href=""><i class="fa-solid fa-bag-shopping"></i></a>
+      </div>
+      <span class="line_div">|</span>
+      <div>
+        <a href=""><i class="fa-solid fa-user"></i> Login</a>
+      </div>
+    </div>
+  </div>
+  <div id="lower_navbar">
+    <ul id="list">
+      <li><a href="">SALE</a></li>
+      <li>
+        <a href="">MAKEUP</a>
+        <div class="make_up">
+          <ul>
+            <li><a href=""></a><b>FACE</b></li>
+            <li><a href=""></a>Foundation</li>
+            <li><a href=""></a>BB & CC Cream</li>
+            <li><a href=""></a>Concealer</li>
+            <li><a href=""></a>Face Primer</li>
+            <li><a href=""></a>Highlighter</li>
+            <li><a href=""></a>Face Brushes</li>
+            <li><a href=""></a>Makeup Palette</li>
+          </ul>
+          <ul>
+            <li><a href=""></a><b>EYE</b></li>
+            <li><a href=""></a>Eye Palettes</li>
+            <li><a href=""></a>Mascara</li>
+            <li><a href=""></a>Eyeliner</li>
+            <li><a href=""></a>Eyebrow</li>
+            <li><a href=""></a>Eyeshadow</li>
+            <li><a href=""></a>Eye Primer</li>
+            <li><a href=""></a>Eye Brushes</li>
+            <li><a href=""></a>Contact Lenses</li>
+          </ul>
+          <ul>
+            <li><a href=""></a><b>LIP</b></li>
+            <li><a href=""></a>Lipstick</li>
+            <li><a href=""></a>Lip Stain</li>
+            <li><a href=""></a>Lip Gloss</li>
+            <li><a href=""></a>Lip Liner</li>
+            <li><a href=""></a>Lip Balm & Treatment</li>
+            <li><a href=""></a>Lip Brushes</li>
+          </ul>
+          <ul>
+            <li><a href=""></a><b>CHEEK</b></li>
+            <li><a href=""></a>Blush</li>
+            <li><a href=""></a>Bronzer</li>
+            <li><a href=""></a>Highlighter</li>
+            <li><a href=""></a>Face Oils</li>
+          </ul>
+          <ul>
+            <li><a href=""></a><b>NAIL MAKEUP</b></li>
+            <li><a href=""></a>Nail Polish</li>
+            <li><a href=""></a>Nail Care</li>
+            <li><a href=""></a>Manicure & Pedicure Tools</li>
+          </ul>
+          <ul>
+            <li><a href=""></a><b>MAKEUP ACCESSORIES</b></li>
+            <li><a href=""></a>Tweezers & Eyebrow Tools</li>
+            <li><a href=""></a>Makeup Removers</li>
+            <li><a href=""></a>Sponges & Applicators</li>
+            <li><a href=""></a>Makeup Bags & Travel Cases</li>
+          </ul>
+
+          <ul>
+            <li><a href=""></a><b>VEGAN</b></li>
+            <li><a href=""></a>Eye</li>
+            <li><a href=""></a>Face</li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <a href="">SKINCARE</a>
+        <div class="skin_care">
+          <ul>
+            <li>
+              <b>MOISTURISERS</b>
+            </li>
+            <li>Moisturisers</li>
+            <li>Night Creams</li>
+            <li>Mists & Essences</li>
+            <li>BB & CC Creams</li>
+          </ul>
+
+          <ul>
+            <li>
+              <b>CLEANSERS</b>
+            </li>
+            <li>Face Wash & Cleansers</li>
+            <li>Removers</li>
+            <li>Face Wipes</li>
+            <li>Toners</li>
+            <li>Hand Sanitisers</li>
+          </ul>
+
+          <ul>
+            <li><b>TREATMENTS & HIGH TECH TOOLS</b></li>
+            <li>Face Serums</li>
+            <li>Cleansing Tools</li>
+            <li>Eye Creams & Treatments</li>
+          </ul>
+
+          <ul>
+            <li><b>MASKS</b></li>
+            <li>Face Masks</li>
+            <li>Sheet Masks</li>
+            <li>Eyes Masks</li>
+            <li>Nose Masks & Strips</li>
+            <li>Lips Masks</li>
+            <li>Hair Masks</li>
+            <li>Hand Masks</li>
+            <li>Foot Masks</li>
+          </ul>
+
+          <ul>
+            <li><b>BATH & SHOWER</b></li>
+            <li>Body Wash & Shower Gel</li>
+            <li>Scrub and Exfoliants</li>
+            <li>Exfoliators</li>
+          </ul>
+
+          <ul>
+            <li><b>BODY MOISTURISERS</b></li>
+            <li>Body Lotions & Body Oils</li>
+            <li>Hand Cream & Foot Cream</li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <a href="">FRAGRANCE</a>
+        <div class="fragrance">
+          <ul>
+            <li><b>WOMEN</b></li>
+            <li>Perfume</li>
+            <li>Mists and Deodorants</li>
+          </ul>
+          <ul>
+            <li><b>MEN</b></li>
+            <li>Perfume</li>
+            <li>BodySprays and Deodorants</li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <a href="">HAIRCARE</a>
+        <div class="hair">
+          <ul>
+            <li><b>SHAMPOO AND CONDITIONER</b></li>
+            <li>Shampoo</li>
+            <li>Conditioner</li>
+          </ul>
+
+          <ul>
+            <li><b>HAIRSTYLING AND TREATMENTS</b></li>
+            <li>Hair Spray & Styling Products</li>
+            <li>Hair Oil</li>
+            <li>Hair Masks</li>
+            <li>Hair Clips</li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <a href="">TOOLS & BRUSHES</a>
+        <div class="tools_brushes">
+          <ul>
+            <li><b>BRUSHES</b></li>
+            <li>Face Brush</li>
+            <li>Eye Brushes</li>
+            <li>Lip Brushes</li>
+          </ul>
+
+          <ul>
+            <li><b>TOOLS</b></li>
+            <li>Sponges & Applicators</li>
+            <li>Face Tools</li>
+            <li>Hair Clips</li>
+            <li>Sharpeners</li>
+            <li>Tweezers & Eyebrow Tools</li>
+            <li>Eyelash Curlers</li>
+            <li>Accessories</li>
+            <li>Brush Cleaners</li>
+          </ul>
+          <ul>
+            <li><b>VEGAN</b></li>
+            <li>Face Brush</li>
+            <li>Eye Brush</li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <a href="">BRANDS</a>
+        <div class="brand">
+          <ul>
+            <li><b>TOP MAKEUP BRANDS</b></li>
+            <li>Sephora Collection</li>
+            <li>Lancôme</li>
+            <li>Huda Beauty</li>
+            <li>Benefit Cosmetics</li>
+            <li>Anastasia Beverly Hills</li>
+            <li>Bobbi Brown</li>
+            <li>Smashbox</li>
+            <li>Estee Lauder</li>
+            <li>Nudestix</li>
+            <li>MAC</li>
+            <li>Make Up For Ever</li>
+            <li>Guerlain</li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+    <div id="line"></div>
+  </div>`
+}
+
+function navbarcss(){
+  return `<style>
+  * {
+  margin: 0px;
+  padding: 0px;
+}
+
+#upper_navbar {
+  display: flex;
+  width: 100%;
+  /* border: 1px solid green; */
+  height: 70px;
+  background-color: #f3f3f3;
+  align-items: center;
+  padding-right: 10px;
+  justify-content: space-around;
+}
+.nav_atri {
+  color: #000000;
+  font-family: sans-serif;
+  font-weight: lighter;
+  font-size: 14px;
+}
+.nav_atri a {
+  text-decoration: none;
+  color: #000000;
+  display: flex;
+}
+.nav_atri a div {
+  padding-left: 8px;
+  padding-right: 8px;
+}
+
+.line_div {
+  color: #d6d6d6;
+}
+
+/* middle nav bar */
+
+#middle_navbar {
+  /* border: 1px solid red; */
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+#logos {
+  /* border: 1px solid green; */
+  display: flex;
+  justify-content: space-between;
+}
+#logos div {
+  padding-right: 30px;
+  padding-left: 5px;
+}
+#logos div a {
+  text-decoration: none;
+  color: #000000;
+  justify-content: center;
+  align-items: center;
+}
+#search input {
+  background: none;
+  border: none;
+  outline: none;
+}
+/* lower nav bar */
+#lower_navbar {
+  /* border: 1px solid green; */
+  height: 50px;
+}
+
+#list {
+  display: flex;
+  list-style: none;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 15px;
+}
+#list li a {
+  text-decoration: none;
+  color: #000000;
+  font-size: 14px;
+  font-weight: bold;
+  font-family: sans-serif;
+}
+#line {
+  border: 0.5px solid #d6d6d6;
+  background-color: #d6d6d6;
+  margin-top: 10px;
+}
+/* #list li :hover {
+  color: #ff369a;
+} */
+#list ul > li:hover {
+  color: #ff369a;
+}
+
+.make_up {
+  display: none;
+}
+.make_up li {
+  list-style: none;
+  margin-top: 15px;
+  margin-bottom: -15px;
+  padding: 10px;
+  align-items: center;
+  cursor: pointer;
+}
+
+#lower_navbar #list > li:hover .make_up {
+  position: absolute;
+  display: flex;
+}
+
+#lower_navbar #list .make_up ul > li:hover {
+  color: #ff369a;
+}
+
+/* skin care list */
+.skin_care {
+  display: none;
+}
+.skin_care li {
+  list-style: none;
+  margin-top: 15px;
+  margin-bottom: -15px;
+  padding: 10px;
+  align-items: flex-start;
+  cursor: pointer;
+}
+#lower_navbar #list > li:hover .skin_care {
+  position: absolute;
+  display: flex;
+  align-items: flex-start;
+}
+
+.fragrance {
+  display: none;
+}
+
+.fragrance li {
+  list-style: none;
+  margin-top: 15px;
+  margin-bottom: -15px;
+  padding: 10px;
+  align-items: flex-start;
+  cursor: pointer;
+}
+#lower_navbar #list > li:hover .fragrance {
+  position: absolute;
+  display: flex;
+  align-items: flex-start;
+}
+
+.hair {
+  display: none;
+}
+.hair li {
+  list-style: none;
+  margin-top: 15px;
+  margin-bottom: -15px;
+  padding: 10px;
+  align-items: flex-start;
+  cursor: pointer;
+}
+#lower_navbar #list > li:hover .hair {
+  position: absolute;
+  display: flex;
+  align-items: flex-start;
+}
+
+.tools_brushes {
+  display: none;
+}
+
+.tools_brushes li {
+  list-style: none;
+  margin-top: 15px;
+  margin-bottom: -15px;
+  padding: 10px;
+  align-items: flex-start;
+  cursor: pointer;
+}
+#lower_navbar #list > li:hover .tools_brushes {
+  position: absolute;
+  display: flex;
+  align-items: flex-start;
+}
+.brand {
+  display: none;
+}
+.brand li {
+  list-style: none;
+  display: table;
+  margin-top: 15px;
+  margin-bottom: -15px;
+  padding: 10px;
+  align-items: flex-start;
+  cursor: pointer;
+}
+#lower_navbar #list > li:hover .brand {
+  display: flex;
+  position: absolute;
+  align-items: flex-start;
+}
+
+@media all and (min-width: 768px) and (max-width: 1024px) {
+  * {
+    margin: 0px;
+    padding: 0px;
+  }
+
+  #upper_navbar {
+    display: flex;
+    width: 100%;
+    /* border: 1px solid green; */
+    height: 70px;
+    background-color: #f3f3f3;
+    align-items: center;
+    padding-right: 10px;
+    justify-content: space-around;
+  }
+  .nav_atri {
+    color: #000000;
+    font-family: sans-serif;
+    font-weight: lighter;
+    font-size: 14px;
+  }
+  .nav_atri a {
+    text-decoration: none;
+    color: #000000;
+    display: flex;
+  }
+  .nav_atri a div {
+    padding-left: 8px;
+    padding-right: 8px;
+  }
+
+  .line_div {
+    color: #d6d6d6;
+  }
+
+  /* middle nav bar */
+
+  #middle_navbar {
+    /* border: 1px solid red; */
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  #logos {
+    /* border: 1px solid green; */
+    display: flex;
+    justify-content: space-between;
+  }
+  #logos div {
+    padding-right: 30px;
+    padding-left: 5px;
+  }
+  #logos div a {
+    text-decoration: none;
+    color: #000000;
+    justify-content: center;
+    align-items: center;
+  }
+  #search input {
+    background: none;
+    border: none;
+    outline: none;
+  }
+  /* lower nav bar */
+  #lower_navbar {
+    /* border: 1px solid green; */
+    height: 50px;
+  }
+
+  #list {
+    display: flex;
+    list-style: none;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 15px;
+  }
+  #list li a {
+    text-decoration: none;
+    color: #000000;
+    font-size: 14px;
+    font-weight: bold;
+    font-family: sans-serif;
+  }
+  #line {
+    border: 0.5px solid #d6d6d6;
+    background-color: #d6d6d6;
+    margin-top: 10px;
+  }
+  /* #list li :hover {
+  color: #ff369a;
+} */
+  #list ul > li:hover {
+    color: #ff369a;
+  }
+
+  .make_up {
+    display: none;
+  }
+  .make_up li {
+    list-style: none;
+    margin-top: 15px;
+    margin-bottom: -15px;
+    padding: 10px;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  #lower_navbar #list > li:hover .make_up {
+    position: absolute;
+    display: flex;
+  }
+
+  #lower_navbar #list .make_up ul > li:hover {
+    color: #ff369a;
+  }
+
+  /* skin care list */
+  .skin_care {
+    display: none;
+  }
+  .skin_care li {
+    list-style: none;
+    margin-top: 15px;
+    margin-bottom: -15px;
+    padding: 10px;
+    align-items: flex-start;
+    cursor: pointer;
+  }
+  #lower_navbar #list > li:hover .skin_care {
+    position: absolute;
+    display: flex;
+    align-items: flex-start;
+  }
+
+  .fragrance {
+    display: none;
+  }
+
+  .fragrance li {
+    list-style: none;
+    margin-top: 15px;
+    margin-bottom: -15px;
+    padding: 10px;
+    align-items: flex-start;
+    cursor: pointer;
+  }
+  #lower_navbar #list > li:hover .fragrance {
+    position: absolute;
+    display: flex;
+    align-items: flex-start;
+  }
+
+  .hair {
+    display: none;
+  }
+  .hair li {
+    list-style: none;
+    margin-top: 15px;
+    margin-bottom: -15px;
+    padding: 10px;
+    align-items: flex-start;
+    cursor: pointer;
+  }
+  #lower_navbar #list > li:hover .hair {
+    position: absolute;
+    display: flex;
+    align-items: flex-start;
+  }
+
+  .tools_brushes {
+    display: none;
+  }
+
+  .tools_brushes li {
+    list-style: none;
+    margin-top: 15px;
+    margin-bottom: -15px;
+    padding: 10px;
+    align-items: flex-start;
+    cursor: pointer;
+  }
+  #lower_navbar #list > li:hover .tools_brushes {
+    position: absolute;
+    display: flex;
+    align-items: flex-start;
+  }
+  .brand {
+    display: none;
+  }
+  .brand li {
+    list-style: none;
+    display: table;
+    margin-top: 15px;
+    margin-bottom: -15px;
+    padding: 10px;
+    align-items: flex-start;
+    cursor: pointer;
+  }
+  #lower_navbar #list > li:hover .brand {
+    display: flex;
+    position: absolute;
+    align-items: flex-start;
+  }
+}
+  </style>
+  `
+ 
+   
+
+
+}
+
+function fonts(){
+    return ` <script
+    src="https://kit.fontawesome.com/24c494a6b6.js"
+    crossorigin="anonymous"
+  ></script>`
+}
+export {navbar,navbarcss,fonts};
+
+//code to be copied in individual html pages , is below
+
+// <script type="module">
+//   import { navbar, navbarcss, fonts } from "./components/navbar.js";
+//   document.body.innerHTML = navbar();
+//   document.head.innerHTML = fonts();
+//   document.head.innerHTML = navbarcss();
+// </script>
+
